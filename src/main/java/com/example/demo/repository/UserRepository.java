52c791ace6cc6fcd8f53b741a66c6c069976c6dd
jiwon Moon <mon99745@dreamsecurity.com>
2024-05-20 14:32:11 +0900

    chore. 미사용 코드 삭제

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -11,6 +11,4 @@ import java.util.Optional;
 public interface UserRepository
 		extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
-
-	Optional<User> findUserByUsernameAndProvider(String username, String provider);
 }
